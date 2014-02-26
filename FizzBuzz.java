88dad635b58262127316538b8c49cc0d2d463f52
Mahbub <mahbubzaman17@gmail.com>
2014-02-26 22:06:45 +0600

    file output

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,28 +1,45 @@
-public class FizzBuzz {
-    
-    public static void main (String args []) {
-        
-        String s;
-        
-        for (int i = 1; i <= 100; ++i) {
-            
-            s = "";
-             
-            if (i % 3 == 0 && i % 5 == 0)
-                s = "FizzBuzz";
-            else if (i % 3 == 0) 
-                s = "Fizz";
-            else if (i % 5 == 0) 
-                 s = "Buzz";
-            else 
-                 s = ""+i;
-            
-            System.out.println(s);
+import java.io.*;
 
-            
-        
+public class FizzBuzz {
+
+    public static void main (String args []) {
+
+
+        try {
+
+            File file = new File ("out.txt");
+            BufferedWriter output = new BufferedWriter(new FileWriter(file));
+            String s;
+
+            for (int i = 1; i <= 100; ++i) {
+
+                s = "";
+
+                if (i % 3 == 0 && i % 5 == 0)
+                    s = "FizzBuzz";
+                else if (i % 3 == 0)
+                    s = "Fizz";
+                else if (i % 5 == 0)
+                    s = "Buzz";
+                else
+                    s = "" + i;
+
+                output.write(s);
+                if (i != 100)
+                    output.newLine();
+            }
+
+            output.close();
+
+            // System.out.println(s);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        
+
+
+
+
     }
- 
+
 }
+
